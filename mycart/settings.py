a96e31146685c484fe3c0cb3aca627f3c00ba95e
Richal06 <112752643+Richal06@users.noreply.github.com>
2024-01-11 14:49:35 +0530

    Update settings.py

--- mycart/settings.py
+++ mycart/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop','login','graph','hospital'
+    'shop', 'login', 'graph', 'hospital'
 ]
 
 MIDDLEWARE = [
@@ -82,7 +82,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -101,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -113,20 +111,19 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL= "/static/"
-# managing media
-MEDIA_ROOT= os.path.join(BASE_DIR, "media")
-MEDIA_URL="/media/"
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-RAZOR_KEY_ID = '*'
-RAZOR_KEY_SECRET = '*'
+
+# Managing media
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+
+RAZOR_KEY_ID = '*'  # Replace with your actual key ID
+RAZOR_KEY_SECRET = '*'  # Replace with your actual key secret
